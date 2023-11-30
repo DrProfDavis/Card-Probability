f04b1066db8caac2fa8456fdd9ed6dcf01f55685
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-11-30 04:18:44 -0600

    bed time

--- calculator.js
+++ calculator.js
@@ -1,19 +1,26 @@
 function calculateProbability() {
     const totalDraws = document.getElementById('totalDraws').value;
-    const successfulDraw = document.getElementById('successfulDraw').value;
+    const successfulDraws = document.getElementById('successfulDraws').value;
     const cardReplacement = document.getElementById('cardReplacement').value.toUpperCase();
     const selectedCard = document.getElementById('selectedCard').value;
     const deckSize = 52;
     const numOfEachCard = 4;
 
-    
-    if (cardReplacement=='YES') {
-        document.getElementById('result').innerText = `The probability of drawing ${successfulDraw} ${selectedCard}'s within ${totalDraws} draws with card replacement:`;
-    }
-    else if (cardReplacement=='NO') {
-        document.getElementById('result').innerText = `The probability of drawing ${successfulDraw} ${selectedCard}'s within ${totalDraws} draws without card replacement:`;
-    }
-    else {
-        document.getElementById('result').innerText = `Please type either Yes or No for card replacement`
+    if (successfulDraws > 4) {
+        document.getElementById('result').innerText = `You cannot have more than 4 successful draws`
+    } else {
+        if (totalDraws < successfulDraws) {
+            document.getElementById('result').innerText = `You cannot have more successful draws than total draws`
+        } else {
+            if (cardReplacement=='YES') {
+                document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}'s within ${totalDraws} draws with card replacement:`;
+            }
+            else if (cardReplacement=='NO') {
+                document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}'s within ${totalDraws} draws without card replacement:`;
+            }
+            else {
+                document.getElementById('result').innerText = `Please type either Yes or No for card replacement`
+            }
+        }
     }
 }
