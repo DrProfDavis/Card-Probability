265c9d07ee190d3b605c9e012911e68777bc07d1
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-11-30 12:49:58 -0600

    first pulls working

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ function calculateProbability() {
     const selectedCard = document.getElementById('selectedCard').value.toUpperCase();
     const deckSize = 52;
 
-    if (selectedCard=='SPADES'||'HEARTS'||'CLUBS'||'DIAMONDS') {
+    if (selectedCard == 'SPADES' || 'HEARTS' || 'CLUBS' || 'DIAMONDS' || 'SPADE' || 'HEART' || 'CLUB' || 'DIAMOND') {
         let num = 13
         if (successfulDraws > 4) {
             document.getElementById('result').innerText = `You cannot have more than 4 successful draws`
@@ -13,19 +13,27 @@ function calculateProbability() {
             if (totalDraws < successfulDraws) {
                 document.getElementById('result').innerText = `You cannot have more successful draws than total draws`
             } else {
-                if (cardReplacement=='YES') {
+                if (cardReplacement == 'NO') {
                     if (totalDraws > 6) {
                         document.getElementById('result').innerText = `Please only have 6 or fewer total draws`
                     } else {
-                        document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard} in ${totalDraws} draw with card replacement:`;
-                        if (successfulDraws==1) {
-                            if (totalDraws==1) {
-                                document.getElementById('stats').innerText = `(${num}/${deckSize}) = ` + (num/deckSize);
+                        document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}(s) in ${totalDraws} draw(s) without card replacement:`;
+                        if (totalDraws == 1) {
+                            document.getElementById('stats').innerText = `(${num}/${deckSize}) = ` + (num / deckSize);
+                        } else if (totalDraws == 2) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize - num}/${deckSize - 1}) = ` + (num / deckSize) * ((deckSize - num) / (deckSize - 1));
+                            }
+                        } else if (totalDraws == 3) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2));
                             }
                         }
                     }
                 }
-                else if (cardReplacement=='NO') {
+                else if (cardReplacement == 'YES') {
                     // document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}'s within ${totalDraws} draws without card replacement:`;
                 }
                 else {
