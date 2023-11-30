6a6899e284aafb0a4932741ca81aa0cc7fc68a9d
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-11-30 13:40:15 -0600

    Exact logic working

--- calculator.js
+++ calculator.js
@@ -9,15 +9,18 @@ function calculateProbability() {
         let num = 13
         if (successfulDraws > 4) {
             document.getElementById('result').innerText = `You cannot have more than 4 successful draws`
+            document.getElementById('stats').innerText = ``
         } else {
             if (totalDraws < successfulDraws) {
                 document.getElementById('result').innerText = `You cannot have more successful draws than total draws`
+                document.getElementById('stats').innerText = ``
             } else {
                 if (cardReplacement == 'NO') {
                     if (totalDraws > 6) {
                         document.getElementById('result').innerText = `Please only have 6 or fewer total draws`
+                        document.getElementById('stats').innerText = ``
                     } else {
-                        document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}(s) in ${totalDraws} draw(s) without card replacement:`;
+                        document.getElementById('result').innerText = `The probability of drawing exactly ${successfulDraws} ${selectedCard}(s) in ${totalDraws} draw(s) without card replacement:`;
                         if (totalDraws == 1) {
                             document.getElementById('stats').innerText = `(${num}/${deckSize}) = ` + (num / deckSize);
                         } else if (totalDraws == 2) {
@@ -44,6 +47,26 @@ function calculateProbability() {
                             } else {
                                 document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3));
                             }
+                        } else if (totalDraws == 5) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2})*(${deckSize-num-2}/${deckSize-3})*(${deckSize-num-3}/${deckSize-4}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2))*((deckSize-num-2)/(deckSize-3))*((deckSize-num-3)/(deckSize-4));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2})*(${deckSize-num-1}/${deckSize-3})*(${deckSize-num-2}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2))*((deckSize-num-1)/(deckSize-3))*((deckSize-num-2)/(deckSize-4));
+                            } else if (successfulDraws == 3) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${deckSize-num}/${deckSize-3})*(${deckSize-num-1}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((deckSize-num)/(deckSize-3))*((deckSize-num-1)/(deckSize-4));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3})*(${deckSize-num}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3))*((deckSize-num)/(deckSize-4));
+                            }
+                        } else {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2})*(${deckSize-num-2}/${deckSize-3})*(${deckSize-num-3}/${deckSize-4})*(${deckSize-num-4}/${deckSize-5}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2))*((deckSize-num-2)/(deckSize-3))*((deckSize-num-3)/(deckSize-4))*((deckSize-num-4)/(deckSize-5));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2})*(${deckSize-num-1}/${deckSize-3})*(${deckSize-num-2}/${deckSize-4})*(${deckSize-num-3}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2))*((deckSize-num-1)/(deckSize-3))*((deckSize-num-2)/(deckSize-4))*((deckSize-num-3)/(deckSize-5));
+                            } else if (successfulDraws == 3) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${deckSize-num}/${deckSize-3})*(${deckSize-num-1}/${deckSize-4})*(${deckSize-num-2}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((deckSize-num)/(deckSize-3))*((deckSize-num-1)/(deckSize-4))*((deckSize-num-2)/(deckSize-5));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3})*(${deckSize-num}/${deckSize-4})*(${deckSize-num-1}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3))*((deckSize-num)/(deckSize-4))*((deckSize-num-1)/(deckSize-5));
+                            }
                         }
                     }
                 }
@@ -55,8 +78,78 @@ function calculateProbability() {
                 }
             }
         }
-    } else {
+    } else if (selectedCard == 'A'||'ACE'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'10'||'J'||'JACK'||'Q'||'QUEEN'||'K'||'KING') {
         let num = 4
-
+        if (successfulDraws > 4) {
+            document.getElementById('result').innerText = `You cannot have more than 4 successful draws`
+            document.getElementById('stats').innerText = ``
+        } else {
+            if (totalDraws < successfulDraws) {
+                document.getElementById('result').innerText = `You cannot have more successful draws than total draws`
+                document.getElementById('stats').innerText = ``
+            } else {
+                if (cardReplacement == 'NO') {
+                    if (totalDraws > 6) {
+                        document.getElementById('result').innerText = `Please only have 6 or fewer total draws`
+                        document.getElementById('stats').innerText = ``
+                    } else {
+                        document.getElementById('result').innerText = `The probability of drawing exactly ${successfulDraws} ${selectedCard}(s) in ${totalDraws} draw(s) without card replacement:`;
+                        if (totalDraws == 1) {
+                            document.getElementById('stats').innerText = `(${num}/${deckSize}) = ` + (num / deckSize);
+                        } else if (totalDraws == 2) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize - 1}) = ` + (num / deckSize) * ((num-1) / (deckSize - 1));
+                            }
+                        } else if (totalDraws == 3) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2));
+                            }
+                        } else if (totalDraws == 4) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2})*(${deckSize-num-2}/${deckSize-3}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2))*((deckSize-num-2)/(deckSize-3));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2})*(${deckSize-num-1}/${deckSize-3}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2))*((deckSize-num-1)/(deckSize-3));
+                            } else if (successfulDraws == 3) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${deckSize-num}/${deckSize-3}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((deckSize-num)/(deckSize-3));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3));
+                            }
+                        } else if (totalDraws == 5) {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2})*(${deckSize-num-2}/${deckSize-3})*(${deckSize-num-3}/${deckSize-4}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2))*((deckSize-num-2)/(deckSize-3))*((deckSize-num-3)/(deckSize-4));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2})*(${deckSize-num-1}/${deckSize-3})*(${deckSize-num-2}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2))*((deckSize-num-1)/(deckSize-3))*((deckSize-num-2)/(deckSize-4));
+                            } else if (successfulDraws == 3) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${deckSize-num}/${deckSize-3})*(${deckSize-num-1}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((deckSize-num)/(deckSize-3))*((deckSize-num-1)/(deckSize-4));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3})*(${deckSize-num}/${deckSize-4}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3))*((deckSize-num)/(deckSize-4));
+                            }
+                        } else {
+                            if (successfulDraws == 1) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${deckSize-num}/${deckSize-1})*(${deckSize-num-1}/${deckSize-2})*(${deckSize-num-2}/${deckSize-3})*(${deckSize-num-3}/${deckSize-4})*(${deckSize-num-4}/${deckSize-5}) = ` + (num / deckSize)*((deckSize-num)/(deckSize-1))*((deckSize-num-1)/(deckSize-2))*((deckSize-num-2)/(deckSize-3))*((deckSize-num-3)/(deckSize-4))*((deckSize-num-4)/(deckSize-5));
+                            } else if (successfulDraws == 2) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${deckSize-num}/${deckSize-2})*(${deckSize-num-1}/${deckSize-3})*(${deckSize-num-2}/${deckSize-4})*(${deckSize-num-3}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((deckSize-num)/(deckSize-2))*((deckSize-num-1)/(deckSize-3))*((deckSize-num-2)/(deckSize-4))*((deckSize-num-3)/(deckSize-5));
+                            } else if (successfulDraws == 3) {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${deckSize-num}/${deckSize-3})*(${deckSize-num-1}/${deckSize-4})*(${deckSize-num-2}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((deckSize-num)/(deckSize-3))*((deckSize-num-1)/(deckSize-4))*((deckSize-num-2)/(deckSize-5));
+                            } else {
+                                document.getElementById('stats').innerText = `(${num}/${deckSize})*(${num-1}/${deckSize-1})*(${num-2}/${deckSize-2})*(${num-3}/${deckSize-3})*(${deckSize-num}/${deckSize-4})*(${deckSize-num-1}/${deckSize-5}) = ` + (num / deckSize)*((num-1)/(deckSize-1))*((num-2)/(deckSize-2))*((num-3)/(deckSize-3))*((deckSize-num)/(deckSize-4))*((deckSize-num-1)/(deckSize-5));
+                            }
+                        }
+                    }
+                }
+                else if (cardReplacement == 'YES') {
+                    // document.getElementById('result').innerText = `The probability of drawing ${successfulDraws} ${selectedCard}'s within ${totalDraws} draws without card replacement:`;
+                }
+                else {
+                    document.getElementById('result').innerText = `Please type either Yes or No for card replacement`
+                }
+            }
+        }
     }
 }
