7da1bba2ece0eb550172884c9623d005f4e6629d
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-12-04 06:47:02 -0600

    v2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let drawValues = {};
 let totalDraws = 0;
 let currentDrawPile = [];
+let mathStringTop = '';
+let mathStringMid = '';
+let mathStringBot = '';
 const cardChoices = {
     ChooseCard: { cardName: 'Choose a Card'},
-    AnyCard: { cardName: 'Any Card'},
+    AnyCard: { cardName: 'Any (Not Working)'},
     Spade: { cardName: 'Spade'},
     Heart: { cardName: 'Heart'},
     Diamond: { cardName: 'Diamond'},
@@ -90,6 +93,10 @@ function calculate() {
     let math = 1;
     let denominator = 1;
     let roundCardsArray = [];
+    mathStringTop = ' # of C = ';
+    mathStringMid = ' = ';
+    mathStringBot = ' = ';
+    
 
     // TODO Must refresh page cause of this line of code
     currentDrawPile = cardChoices;
@@ -213,9 +220,9 @@ function calculate() {
         return a - b;
     });
     let finalArray = [...new Set(roundCardsArray)]
-    console.log(`finalArray = ` + finalArray)
+    // console.log(`finalArray = ` + finalArray)
     for (let i = 0; i < finalArray.length; i++) {
-        console.log(`finalArray[${i}] = ` + finalArray[i])
+        // console.log(`finalArray[${i}] = ` + finalArray[i])
         switch (finalArray[i]) {
             case 0:
                 math = math * spade();
@@ -294,114 +301,180 @@ function calculate() {
         }
     }
 
-    denominator = combinationFormula(52, totalDraws)
+    denominator = combinationFormula(52, totalDraws).toFixed(0)
+    denominatorString = `Total # of C = C<sub>Total</sub> = <sub>52</sub>C<sub>${totalDraws}</sub> = ${denominator}`
+    let percentage = ((math / denominator) * 100).toFixed(2);
     console.log(((math / denominator) * 100).toFixed(2));
+    document.getElementById('resultsHeaderContainer').innerHTML = 'Here is your results:';
+    document.getElementById('resultsPercentageContainer').innerHTML = `${percentage}%`;
+    document.getElementById('resultsTopContainer').innerHTML = mathStringTop+mathStringMid+mathStringBot+' = '+math;
+    document.getElementById('resultsMidContainer').innerHTML = denominatorString;
+    document.getElementById('resultsBotContainer').innerHTML = `${math} / ${denominator} = ${(math/denominator).toFixed(4)} = ${percentage}`;
+    document.getElementById('resultsFooterContainer').innerHTML = 'Please refresh page before next calculation';
 }
+
+// TODO More logic to come, I know this seems silly but I have plans for this
 
 let anyCardCount = 0;
 function anyCard() {
-    let c = combinationFormula(52 - totalDraws + anyCardCount, anyCardCount);
+    let s = 52 - totalDraws + anyCardCount
+    let c = combinationFormula(s, anyCardCount);
+    
+    mathStringTop += `( C<sub>Any</sub> )`
+    mathStringMid += `( <sub>${s}</sub>C<sub>${anyCardCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let spadeCount = 0;
 function spade() {
     let c = combinationFormula(13, spadeCount);
+    mathStringTop += `( C<sub>Spade</sub> )`
+    mathStringMid += `( <sub>${13}</sub>C<sub>${spadeCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let heartCount = 0;
 function heart() {
     let c = combinationFormula(13, heartCount);
+    mathStringTop += `( C<sub>Heart</sub> )`
+    mathStringMid += `( <sub>${13}</sub>C<sub>${heartCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let clubCount = 0;
 function club() {
     let c = combinationFormula(13, clubCount);
+    mathStringTop += `( C<sub>Club</sub> )`
+    mathStringMid += `( <sub>${13}</sub>C<sub>${clubCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let diamondCount = 0;
 function diamond() {
     let c = combinationFormula(13, diamondCount);
+    mathStringTop += `( C<sub>Diamond</sub> )`
+    mathStringMid += `( <sub>${13}</sub>C<sub>${diamondCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let aceCount = 0;
 function ace() {
     let c = combinationFormula(4, aceCount);
+    mathStringTop += `( C<sub>Ace</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${aceCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let twoCount = 0;
 function two() {
     let c = combinationFormula(4, twoCount);
+    mathStringTop += `( C<sub>Two</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${twoCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let threeCount = 0;
 function three() {
     let c = combinationFormula(4, threeCount);
+    mathStringTop += `( C<sub>Three</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${threeCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let fourCount = 0;
 function four() {
     let c = combinationFormula(4, fourCount);
+    mathStringTop += `( C<sub>Four</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${fourCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let fiveCount = 0;
 function five() {
     let c = combinationFormula(4, fiveCount);
+    mathStringTop += `( C<sub>Five</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${fiveCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let sixCount = 0;
 function six() {
     let c = combinationFormula(4, sixCount);
+    mathStringTop += `( C<sub>Six</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${sixCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let sevenCount = 0;
 function seven() {
     let c = combinationFormula(4, sevenCount);
+    mathStringTop += `( C<sub>Seven</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${sevenCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let eightCount = 0;
 function eight() {
     let c = combinationFormula(4, eightCount);
+    mathStringTop += `( C<sub>Eight</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${eightCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let nineCount = 0;
 function nine() {
     let c = combinationFormula(4, nineCount);
+    mathStringTop += `( C<sub>Nine</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${nineCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let tenCount = 0;
 function ten() {
     let c = combinationFormula(4, tenCount);
+    mathStringTop += `( C<sub>Ten</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${tenCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let jackCount = 0;
 function jack() {
     let c = combinationFormula(4, jackCount);
+    mathStringTop += `( C<sub>Jack</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${jackCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let queenCount = 0;
 function queen() {
     let c = combinationFormula(4, queenCount);
+    mathStringTop += `( C<sub>Queen</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${queenCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
 
 let kingCount = 0;
 function king() {
     let c = combinationFormula(4, kingCount);
+    mathStringTop += `( C<sub>King</sub> )`
+    mathStringMid += `( <sub>${4}</sub>C<sub>${kingCount}</sub> )`
+    mathStringBot += `(${c})`
     return c;
 }
